b1ce1de5865f5c86b74ccdbca9a6c5071330f87d
Gautamkalamkar <gautamkalamkar4@gmail.com>
2022-10-09 20:01:52 -0700

    (10th oct 2022)

---
+++ sum-of-two-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, sum;
+
+    printf("Enter the First number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    printf("The sum of two numbers is %d", (a + b));
+}
