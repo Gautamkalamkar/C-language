45c1af086a80fb4bfc8285257ba6aaf9e53e98ae
Gautamkalamkar <gautamkalamkar4@gmail.com>
2022-11-21 17:08:07 -0800

    21 Nov 2022

---
+++ simple-interest.c
@@ -0,0 +1,16 @@
+// Author: Gautam kalamkar
+
+#include <stdio.h>
+int main()
+{
+    int prin, no;
+    float rate, si;
+    prin = 100;
+    no = 3;
+    rate = 8.5;
+
+    si = prin * no * rate / 100;
+    printf("%f", si);
+
+    return 0;
+}
